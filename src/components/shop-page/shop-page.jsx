da9a0bab9cf78045bfe802d31569e11edaa254ac
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-07-14 14:40:07 -0600

    create shop ui

--- src/components/shop-page/shop-page.jsx
+++ src/components/shop-page/shop-page.jsx
@@ -1,11 +1,35 @@
 import { useOutletContext } from "react-router-dom";
+
 import styles from "./shop-page.module.css";
+
+function getImageScaleClass(image) {
+    let imageScaleClass = styles.productCardImgWidthScale;
+    const testImage = document.createElement("img");
+    testImage.src = image;
+    const testArea = document.querySelector("div");
+    testArea.appendChild(testImage);
+    if (testImage.clientHeight > testImage.clientWidth) imageScaleClass = styles.productCardImgHeightScale;
+    testArea.removeChild(testImage);
+
+    return imageScaleClass;
+}
 
 function ShopPage() {
     const products = useOutletContext();
 
     return <div className={styles.shopPage}>
-        <h1>Shop Page</h1>
+        {products.map((product) => {
+            let imageScaleClass = getImageScaleClass(product.image);
+            console.log(imageScaleClass);
+
+
+            return <div key={product.id} className={styles.productCard}>
+            <img src={product.image} alt={product.title} className={imageScaleClass}/>
+            <p className={styles.productCardTitle}>{product.title}</p>
+            <p className={styles.productCardPrice}>${product.price}</p>
+            <p className={styles.productCardRating}>{product.rating.rate} out of 5 Stars</p> 
+            </div>
+        })}
     </div>;
 }
 
