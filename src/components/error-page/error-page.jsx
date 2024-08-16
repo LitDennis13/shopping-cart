140400b4571661421b4ae801d6b04451e625aaeb
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-08-15 22:40:55 -0600

    updated error page to be more helpful

--- src/components/error-page/error-page.jsx
+++ src/components/error-page/error-page.jsx
@@ -1,7 +1,10 @@
+import styles from "./error-page.module.css";
+
 function ErrorPage() {
-    return <>
+    return <div className={styles.errorPage}>
         <h1>Error Page</h1>
-    </>;
+        <p>Either this page does not exist or there is an error (check console for more detail)</p>
+    </div>;
 }
 
 export default ErrorPage;
